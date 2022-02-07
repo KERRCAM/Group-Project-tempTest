6fa7adaa3efe3622f25a8fe7a354b188f0e02e55
rf212690 <RF212690@gmail.com>
2022-02-07 10:43:53 +0000

    w

---
+++ src/com/company/Interface.java
@@ -0,0 +1,10 @@
+package com.company;
+import javax.swing.JFrame;
+
+public class Interface extends JFrame{
+    public Interface(){
+        setSize(600,800);
+        setTitle("Test");
+        setVisible(true);
+    }
+}
