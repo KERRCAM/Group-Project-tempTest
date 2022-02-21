43642411e8938905022713307d0b70a75a9787a4
KERRCAM <kerrcameron@hotmail.com>
2022-02-21 02:58:26 +0000

    attempt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
-        Interface Interface=new Interface(); // makes screen
+        //Interface Interface=new Interface(); // makes screen
+
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(true); // for testing only (true)
+        window.setTitle("game");
+
+        screen screen = new screen();
+        window.add(screen);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        screen.startGameThread();
+
     }
 }
--- src/com/company/screen.java
+++ src/com/company/screen.java
@@ -1,22 +1,71 @@
 package com.company;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.JPanel;
 
-public class screen extends JPanel{
+public class screen extends JPanel implements Runnable{
 
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    Thread gameThread;
+
+
+    public screen(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            //System.out.println("test");
+            //update();
+
+            repaint();
+
+        }
+    /*
+    public void update() {
+
+        }
+    */
+    public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D)g;
+
+            g2.setColor(Color.white);
+            g2.fillRect(100, 100, tileSize, tileSize);
+            g2.dispose();
+        }
+
+
+
+    }
+
+
+    /*
     private int width;
     private int height;
-
     public screen() {
         super();
         width = 500;
         height = 500;
     }
-
+    */
 
 
 
